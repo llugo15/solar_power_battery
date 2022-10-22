59c086fd46ca9140ae10f17361f488c0c864b950
Lauren <llugo@tamu.edu>
2022-10-22 11:33:13 -0500

    Blink and HelloWorld

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -3,22 +3,39 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
 
-#define BLINK_GPIO 13
+#define BLINK_GPIO 5
 
 void hello_task(void)
 {
-    printf("Hello Lauren!\n");
-    for(int i = 10; i >= 0; i--){
-        printf("restarting in %d seconds...\n", i);
+    while(1){
+        printf("Hello World!\n");
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+}
+
+void blinky(void)
+{
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1)
+    {
+        printf("Blink on!\n");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        /*Light On*/
+        printf("Blink off!\n");
+        gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
-    printf("Restarting Now...\n");
-    fflush(stdout);
-    esp_restart();
 }
 
 void app_main(void)
 {
-    hello_task();
+    //xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+    //xTaskCreate(&blinky, "blinky", 512, NULL, 5, NULL);
+    esp_log_level_set("LOGGING", ESP_LOG_INFO);
+    blinky();
 }
